cc65f48696e4aa0f2db5461860dd7433fb62293b
Maksymilian <maksymilian.mazur@maandag.pl>
2023-11-30 11:31:58 +0100

    passwordGen

---
+++ Test/cwicz.py
@@ -0,0 +1,28 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+print(fruits)
+
+
+total = 0
+for number in range(1,101):
+    total +=number
+print(total)
+
+#student_heights = input().split()
+#for n in range(0, len(student_heights)):
+ #   student_heights[n] = int(student_heights[n])
+
+#total_heights = 0
+#for height in student_heights:
+ #   total_heights += height
+#print(f"Total heights = {total_heights}")
+
+#number_of_students = 0
+#for student in student_heights:
+ #   number_of_students += 1
+#print(f"Number of students = {number_of_students}")
+
+#average_height = round(total_heights / number_of_students)
+#print(f"Average height = {average_height}")
